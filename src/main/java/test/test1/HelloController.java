7d4b51f3d04eb9883536816277230872adf17f4c
Clément <clement.hayot@ynov.com>
2025-12-12 10:13:45 +0100

    Début agence

--- src/main/java/test/test1/HelloController.java
+++ src/main/java/test/test1/HelloController.java
@@ -2,13 +2,25 @@ package test.test1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import test.test1.modele.Agence;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-            welcomeText.setText("Welcome to CLIMAgence!");
+    private Label labelTitre;
+
+    @FXML
+    private ListView<String> listViewVoyages;
+
+    private Agence agence;
+
+    @FXML
+    public void initialize() {
+        // créer l'agence et charger les données
+        agence = new Agence("Du Bled à Bali");
+
+        labelTitre.setText("Voyages disponibles - " + agence.getNom());
+        listViewVoyages.getItems().setAll(agence.getVoyages());
     }
 }
--- src/main/java/test/test1/modele/Agence.java
+++ src/main/java/test/test1/modele/Agence.java
@@ -1,5 +1,27 @@
 package test.test1.modele;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agence {
 
+    private String nom;
+    private List<String> voyages; // simplifié pour le test JavaFX
+
+    public Agence(String nom) {
+        this.nom = nom;
+        this.voyages = new ArrayList<>();
+        // données de test
+        voyages.add("Circuit Bali 7j");
+        voyages.add("Séjour Bali 10j");
+        voyages.add("Du Bled à Bali - All Inclusive");
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public List<String> getVoyages() {
+        return voyages;
+    }
 }
